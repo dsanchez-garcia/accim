91d2db6003c464a2a904370bfdd04183be906536
dsanchez-garcia <dsanchez7@us.es>
2021-03-07 15:11:18 +0100

    demand checked and added to outputs

--- tests/test_accim_Base_EMS_v03.py
+++ tests/test_accim_Base_EMS_v03.py
@@ -34,5 +34,20 @@ def test_addEMSProgramsBase():
     
     programlist = ([program.Name for program in idf1.idfobjects['EnergyManagementSystem:Program']])
     assert ('SetComfTemp' in programlist) == True
+
+    SetComfTemp = ([program
+                    for program
+                    in idf1.idfobjects['EnergyManagementSystem:Program']
+                    if program.Name == 'SetComfTemp'])
+
+    assert SetComfTemp[0].Program_Line_1 == 'if AdapStand == 1'
+    assert SetComfTemp[0].Program_Line_2 == 'set ComfTemp = RMOT*0.33+18.8'
+    assert SetComfTemp[0].Program_Line_3 == 'elseif AdapStand == 2'
+    assert SetComfTemp[0].Program_Line_4 == 'set ComfTemp = PMOT*0.31+17.8'
+    assert SetComfTemp[0].Program_Line_5 == 'endif'
+
+
+    print(SetComfTemp[0].Name)
+    print(SetComfTemp[0].Program_Line_1)
     
 test_addEMSProgramsBase()
