03a6c4a1fc51efaa227d98c8dce329f1c8f7ef25
dsanchez-garcia <dsanchez7@us.es>
2021-03-10 18:50:14 +0100

    update

--- accim/sim/accim_Main.py
+++ accim/sim/accim_Main.py
@@ -214,8 +214,38 @@ class accimobj_MultipleZone_Ep94 (accimobj):
 # todo add a accimobj with mz and Ep version in the arguments
 
 
-class accimInstance ():
+class accimInstance():
     """MultipleZone accim object."""
+
+    from os import listdir
+    import numpy
+
+    from accim.sim.accim_IDFgeneration import inputdataSingleZone
+    from accim.sim.accim_IDFgeneration import inputdataMultipleZone
+    from accim.sim.accim_IDFgeneration import genIDFSingleZone
+    from accim.sim.accim_IDFgeneration import genIDFMultipleZone
+
+    from accim.sim.accim_Base import setComfFieldsPeople
+    from accim.sim.accim_Base import addOpTempTherm
+    from accim.sim.accim_Base import addBaseSchedules
+    from accim.sim.accim_Base import setAvailSchOn
+    from accim.sim.accim_Base import saveaccim
+
+    from accim.sim.accim_Base_EMS import addEMSProgramsBase
+    from accim.sim.accim_Base_EMS import addEMSPCMBase
+    from accim.sim.accim_Base_EMS import addEMSOutputVariableBase
+    from accim.sim.accim_Base_EMS import addOutputVariablesTimestep
+    from accim.sim.accim_Base_EMS import addSimplifiedOutputVariables
+
+
+    from accim.sim.accim_SingleZone import addForscriptSchSingleZone
+
+    from accim.sim.accim_SingleZone_EMS import addGlobVarListSingleZone
+    from accim.sim.accim_SingleZone_EMS import addEMSSensorsSingleZone
+    from accim.sim.accim_SingleZone_EMS import addEMSActuatorsSingleZone
+    from accim.sim.accim_SingleZone_EMS import addEMSProgramsSingleZone
+    from accim.sim.accim_SingleZone_EMS import addOutputVariablesSingleZone
+
 
     from accim.sim.accim_MultipleZone import addMultipleZoneSch
     from accim.sim.accim_MultipleZone import addCurveObj
@@ -235,7 +265,7 @@ class accimInstance ():
         from eppy.modeleditor import IDF
         if EnergyPlus_version.lower == 'ep94':
             iddfile = 'C:/EnergyPlusV9-4-0/Energy+.idd'
-        elif EnergyPlus_version.lower == 'ep91':
+        else:
             iddfile = 'C:/EnergyPlusV9-1-0/Energy+.idd'
         IDF.setiddname(iddfile)
 
--- accim/sim/accis.py
+++ accim/sim/accis.py
@@ -500,68 +500,43 @@ def addAccis(
         ScriptType: str = 'MultipleZones',
         Outputs: str = 'Standard',
         EnergyPlus_version: str ='Ep94',
-        AdapStand: list[int] = None,
-        CAT: list[int] = None,
-        ComfMod: list[int] = None,
-        HVACmode: list[int] = None,
-        VentCtrl: list[int] = None,
-        VSToffset: list[float] = 0,
-        MinOToffset: list[float] = 50,
-        MaxWindSpeed: list[float] = 50,
+        AdapStand: any = None,
+        CAT: any = None,
+        ComfMod: any = None,
+        HVACmode: any = None,
+        VentCtrl: any = None,
+        VSToffset: any = 0,
+        MinOToffset: any = 50,
+        MaxWindSpeed: any = 50,
         ASTtol_start: float = 0.1,
         ASTtol_end_input: float = 0.1,
         ASTtol_steps: float = 0.1,
         NameSuffix: str = ''
         ):
     """
-    Add ACCIS based on arguments.
-
-    If no argument entered, information will be asked in command prompt.
-
     Parameters
-    ----------
-    ScriptType : str, optional
-        DESCRIPTION. The default is 'MultipleZones'. Can be 'MultipleZones'or
-        'mz', or 'SingleZone' or 'sz'.
-    Outputs : str, optional
-        DESCRIPTION. The default is 'Standard'. Can be 'Standard',
-        'Simplified' or 'Timestep'.
-    EnergyPlus_version : str, optional
-        DESCRIPTION. The default is 'Ep94'. Can be 'Ep91' or 'Ep94'.
-    AdapStand : list, optional
-        DESCRIPTION. The default is None.(0 = CTE; 1 = EN16798-1; 2 = ASHRAE 55)
-    CAT : list, optional
-        DESCRIPTION. The default is None.(1 = CAT I; 2 = CAT II; 3 = CAT III; 80 = 80% ACCEPT; 90 = 90% ACCEPT)
-    ComfMod : list, optional
-        DESCRIPTION. The default is None.(0 = Static; 1 = OUT-CTE; 2 = OUT-SEN16798/SASHRAE55; 3 = OUT-AEN16798/AASHRAE55)
-    HVACmode : list, optional
-        DESCRIPTION. The default is None.(0 = Fully Air-conditioned; 1 = Naturally ventilated; 2 = Mixed Mode)
-    VentCtrl : list, optional
-        DESCRIPTION. The default is None.(0 = Ventilates above neutral temperature; 1 = Ventilates above upper comfort limit)
-    VSToffset : list, optional
-        DESCRIPTION. The default is 0.
-    MinOToffset : list, optional
-        DESCRIPTION. The default is 50.
-    MaxWindSpeed : list, optional
-        DESCRIPTION. The default is 50.
-    ASTtol_start : float, optional
-        DESCRIPTION. The default is 0.1.
-    ASTtol_end_input : float, optional
-        DESCRIPTION. The default is 0.1.
-    ASTtol_steps : float, optional
-        DESCRIPTION. The default is 0.1.
+    :param ScriptType:
+    :param Outputs:
+    :param EnergyPlus_version:
+    :param AdapStand:
+    :param CAT:
+    :param ComfMod:
+    :param HVACmode:
+    :param VentCtrl:
+    :param VSToffset:
+    :param MinOToffset:
+    :param MaxWindSpeed:
+    :param ASTtol_start:
+    :param ASTtol_end_input:
+    :param ASTtol_steps:
+    :param NameSuffix:
 
-    Raises
-    ------
-    ValueError
-        DESCRIPTION. EnergyPlus version not supported.
+    Exceptions
+            DESCRIPTION. EnergyPlus version not supported.
         Only works for EnergyPlus 9.1 (enter Ep91) and
         EnergyPlus 9.4 (enter Ep94) versions.
 
-    Returns
-    -------
-    None.
-
+    :return:
     """
     import accim.sim.accim_Main as accim_Main
 
@@ -573,7 +548,27 @@ def addAccis(
     filelist = ([file.split('.idf')[0] for file in filelist])
 
     for file in filelist:
-        z = accim_Main.accimInstance(file, ScriptType, EnergyPlus_version)
+        z = accim_Main.accimInstance(filename_temp=file, ScriptType=ScriptType, EnergyPlus_version=EnergyPlus_version)
+
+        # if ScriptType.lower() == 'MultipleZones'.lower() or ScriptType.lower() == 'mz':
+        #     if EnergyPlus_version.lower() == 'ep94':
+        #         z = accim_Main.accimobj_MultipleZone_Ep94(file)
+        #     elif EnergyPlus_version.lower() == 'ep91':
+        #         z = accim_Main.accimobj_MultipleZone_Ep91(file)
+        #     else:
+        #         raise ValueError("""EnergyPlus version not supported.\n
+        #                          Only works for EnergyPlus 9.1 (enter Ep91) and
+        #                          EnergyPlus 9.4 (enter Ep94) versions.""")
+        # elif ScriptType.lower() == 'SingleZone'.lower() or ScriptType.lower() == 'sz':
+        #     if EnergyPlus_version.lower() == 'ep94':
+        #         z = accim_Main.accimobj_SingleZone_Ep94(file)
+        #     elif EnergyPlus_version.lower() == 'ep91':
+        #         z = accim_Main.accimobj_SingleZone_Ep91(file)
+        #     else:
+        #         raise ValueError("""EnergyPlus version not supported.\n
+        #                          Only works for EnergyPlus 9.1 (enter Ep91) and
+        #                          EnergyPlus 9.4 (enter Ep94) versions.""")
+
 
         z.setComfFieldsPeople()
         z.addOpTempTherm()
