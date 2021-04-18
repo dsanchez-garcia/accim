511d915d186cf2da52e4a4dac8af52751e8f6938
dsanchez-garcia <dsanchez7@us.es>
2021-04-18 08:57:56 +0200

    old accis functions and classes superseded

---
+++ accim/sim/SS/accim_Main_SS.py
@@ -0,0 +1,211 @@
+"""Classes for accim SingleZone and MultipleZone."""
+
+
+class accimobj():
+    """accim Object."""
+
+    from os import listdir
+    import numpy
+
+    from accim.sim.accim_IDFgeneration import inputdataSingleZone
+    from accim.sim.accim_IDFgeneration import inputdataMultipleZone
+    from accim.sim.accim_IDFgeneration import genIDFSingleZone
+    from accim.sim.accim_IDFgeneration import genIDFMultipleZone
+
+    from accim.sim.accim_Base import setComfFieldsPeople
+    from accim.sim.accim_Base import addOpTempTherm
+    from accim.sim.accim_Base import addBaseSchedules
+    from accim.sim.accim_Base import setAvailSchOn
+    from accim.sim.accim_Base import saveaccim
+
+    from accim.sim.accim_Base_EMS import addEMSProgramsBase
+    from accim.sim.accim_Base_EMS import addEMSPCMBase
+    from accim.sim.accim_Base_EMS import addEMSOutputVariableBase
+    from accim.sim.accim_Base_EMS import addOutputVariablesTimestep
+    from accim.sim.accim_Base_EMS import addSimplifiedOutputVariables
+
+    pass
+
+
+class accimobj_SingleZone_Ep91 (accimobj):
+    """SingleZone accim object."""
+
+    from accim.sim.accim_SingleZone import addForscriptSchSingleZone
+
+    from accim.sim.accim_SingleZone_EMS import addGlobVarListSingleZone
+    from accim.sim.accim_SingleZone_EMS import addEMSSensorsSingleZone
+    from accim.sim.accim_SingleZone_EMS import addEMSActuatorsSingleZone
+    from accim.sim.accim_SingleZone_EMS import addEMSProgramsSingleZone
+    from accim.sim.accim_SingleZone_EMS import addOutputVariablesSingleZone
+
+    def __init__(self, filename_temp, verboseMode: bool = True):
+        from eppy import modeleditor
+        from eppy.modeleditor import IDF
+
+        iddfile = 'C:/EnergyPlusV9-1-0/Energy+.idd'
+        IDF.setiddname(iddfile)
+
+        fname1 = filename_temp+'.idf'
+        self.idf0 = IDF(fname1)
+        self.idf0.savecopy(filename_temp+'_pymod.idf')
+
+        self.filename = filename_temp+'_pymod'
+        fname1 = self.filename+'.idf'
+        self.idf1 = IDF(fname1)
+        self.filename = filename_temp+'_pymod'
+
+        # print(self.filename)
+
+        self.zonenames_orig = ([zone.Name for zone in self.idf1.idfobjects['ZONE']])
+        # print(self.zonenames_orig)
+
+        self.zonenames = ([sub.replace(':', '_') for sub in ([zone.Name for zone in self.idf1.idfobjects['ZONE']])])
+        # print(self.zonenames)
+        if verboseMode:
+            print(f'The zones in the model {filename_temp} are:')
+            print(*self.zonenames, sep="\n")
+
+class accimobj_MultipleZone_Ep91 (accimobj):
+    """MultipleZone accim object."""
+
+    from accim.sim.accim_MultipleZone import addMultipleZoneSch
+    from accim.sim.accim_MultipleZone import addCurveObj
+    from accim.sim.accim_MultipleZone import addDetHVACobjEp91
+    from accim.sim.accim_MultipleZone import addForscriptSchMultipleZone
+    from accim.sim.accim_MultipleZone import checkVentIsOn
+
+    from accim.sim.accim_MultipleZone_EMS import addGlobVarListMultipleZone
+    from accim.sim.accim_MultipleZone_EMS import addEMSSensorsMultipleZone
+    from accim.sim.accim_MultipleZone_EMS import addEMSActuatorsMultipleZone
+    from accim.sim.accim_MultipleZone_EMS import addEMSProgramsMultipleZone
+    from accim.sim.accim_MultipleZone_EMS import addEMSOutputVariableMultipleZone
+    from accim.sim.accim_MultipleZone_EMS import addOutputVariablesMultipleZone
+
+    def __init__(self, filename_temp, verboseMode: bool = True):
+        from eppy import modeleditor
+        from eppy.modeleditor import IDF
+
+        iddfile = 'C:/EnergyPlusV9-1-0/Energy+.idd'
+        IDF.setiddname(iddfile)
+
+        fname1 = filename_temp+'.idf'
+        self.idf0 = IDF(fname1)
+        self.idf0.savecopy(filename_temp+'_pymod.idf')
+
+        self.filename = filename_temp+'_pymod'
+        fname1 = self.filename+'.idf'
+        self.idf1 = IDF(fname1)
+        self.filename = filename_temp+'_pymod'
+
+        # print(self.filename)
+
+        self.zonenames_orig = ([zone.Name for zone in self.idf1.idfobjects['ZONE']])
+        # print(self.zonenames_orig)
+
+        self.zonenames = ([sub.replace(':', '_') for sub in ([zone.Name for zone in self.idf1.idfobjects['ZONE']])])
+        # print(self.zonenames)
+
+        self.windownamelist_orig = ([window.Name for window in self.idf1.idfobjects['AirflowNetwork:MultiZone:Component:DetailedOpening'] if window.Name.endswith('_Win')])
+        # print(self.windownamelist_orig)
+        self.windownamelist_orig_split = ([i.split('_') for i in self.windownamelist_orig])
+        # print(self.windownamelist_orig_split)
+
+        self.windownamelist = ([sub.replace(':', '_') for sub in ([window.Name for window in self.idf1.idfobjects['AirflowNetwork:MultiZone:Component:DetailedOpening'] if window.Name.endswith('_Win')])])
+        # print(self.windownamelist)
+        if verboseMode:
+            print(f'The zones in the model {filename_temp} are:')
+            print(*self.zonenames, sep="\n")
+            print(f'The windows in the model {filename_temp} are:')
+            print(*self.windownamelist, sep="\n")
+
+class accimobj_SingleZone_Ep94 (accimobj):
+    """SingleZone accim object."""
+
+    from accim.sim.accim_SingleZone import addForscriptSchSingleZone
+
+    from accim.sim.accim_SingleZone_EMS import addGlobVarListSingleZone
+    from accim.sim.accim_SingleZone_EMS import addEMSSensorsSingleZone
+    from accim.sim.accim_SingleZone_EMS import addEMSActuatorsSingleZone
+    from accim.sim.accim_SingleZone_EMS import addEMSProgramsSingleZone
+    from accim.sim.accim_SingleZone_EMS import addOutputVariablesSingleZone
+
+    def __init__(self, filename_temp, verboseMode: bool = True):
+        from eppy import modeleditor
+        from eppy.modeleditor import IDF
+
+        iddfile = 'C:/EnergyPlusV9-4-0/Energy+.idd'
+        IDF.setiddname(iddfile)
+
+        fname1 = filename_temp+'.idf'
+        self.idf0 = IDF(fname1)
+        self.idf0.savecopy(filename_temp+'_pymod.idf')
+
+        self.filename = filename_temp+'_pymod'
+        fname1 = self.filename+'.idf'
+        self.idf1 = IDF(fname1)
+        self.filename = filename_temp+'_pymod'
+
+        # print(self.filename)
+
+        self.zonenames_orig = ([zone.Name for zone in self.idf1.idfobjects['ZONE']])
+        # print(self.zonenames_orig)
+
+        self.zonenames = ([sub.replace(':', '_') for sub in ([zone.Name for zone in self.idf1.idfobjects['ZONE']])])
+        # print(self.zonenames)
+        if verboseMode:
+            print(f'The zones in the model {filename_temp} are:')
+            print(*self.zonenames, sep="\n")
+
+
+class accimobj_MultipleZone_Ep94 (accimobj):
+    """MultipleZone accim object."""
+
+    from accim.sim.accim_MultipleZone import addMultipleZoneSch
+    from accim.sim.accim_MultipleZone import addCurveObj
+    from accim.sim.accim_MultipleZone import addDetHVACobjEp94
+    from accim.sim.accim_MultipleZone import addForscriptSchMultipleZone
+    from accim.sim.accim_MultipleZone import checkVentIsOn
+
+    from accim.sim.accim_MultipleZone_EMS import addGlobVarListMultipleZone
+    from accim.sim.accim_MultipleZone_EMS import addEMSSensorsMultipleZone
+    from accim.sim.accim_MultipleZone_EMS import addEMSActuatorsMultipleZone
+    from accim.sim.accim_MultipleZone_EMS import addEMSProgramsMultipleZone
+    from accim.sim.accim_MultipleZone_EMS import addEMSOutputVariableMultipleZone
+    from accim.sim.accim_MultipleZone_EMS import addOutputVariablesMultipleZone
+
+    def __init__(self, filename_temp, verboseMode: bool = True):
+        from eppy import modeleditor
+        from eppy.modeleditor import IDF
+
+        iddfile = 'C:/EnergyPlusV9-4-0/Energy+.idd'
+        IDF.setiddname(iddfile)
+
+        fname1 = filename_temp+'.idf'
+        self.idf0 = IDF(fname1)
+        self.idf0.savecopy(filename_temp+'_pymod.idf')
+
+        self.filename = filename_temp+'_pymod'
+        fname1 = self.filename+'.idf'
+        self.idf1 = IDF(fname1)
+        self.filename = filename_temp+'_pymod'
+
+        # print(self.filename)
+
+        self.zonenames_orig = ([zone.Name for zone in self.idf1.idfobjects['ZONE']])
+        # print(self.zonenames_orig)
+
+        self.zonenames = ([sub.replace(':', '_') for sub in ([zone.Name for zone in self.idf1.idfobjects['ZONE']])])
+        # print(self.zonenames)
+
+        self.windownamelist_orig = ([window.Name for window in self.idf1.idfobjects['AirflowNetwork:MultiZone:Component:DetailedOpening'] if window.Name.endswith('_Win')])
+        # print(self.windownamelist_orig)
+        self.windownamelist_orig_split = ([i.split('_') for i in self.windownamelist_orig])
+        # print(self.windownamelist_orig_split)
+
+        self.windownamelist = ([sub.replace(':', '_') for sub in ([window.Name for window in self.idf1.idfobjects['AirflowNetwork:MultiZone:Component:DetailedOpening'] if window.Name.endswith('_Win')])])
+        # print(self.windownamelist)
+        if verboseMode:
+            print(f'The zones in the model {filename_temp} are:')
+            print(*self.zonenames, sep="\n")
+            print(f'The windows in the model {filename_temp} are:')
+            print(*self.windownamelist, sep="\n")
---
+++ accim/sim/SS/accis_SS.py
@@ -0,0 +1,495 @@
+"""
+Run any of the functions below to add the accis.
+
+These functions transform fixed setpoint temperature
+building energy models into adaptive setpoint temperature energy models
+by adding the Adaptive Comfort Control Implementation Script (ACCIS)
+"""
+
+
+def addAccisSingleZoneEp91():
+    """Add SingleZone accis."""
+    import accim.sim.accim_Main as accim_Main
+    from os import listdir
+
+    filelist = ([file for file in listdir() if file.endswith('.idf')
+                 and '_pymod' not in file])
+
+    filelist = ([file.split('.idf')[0] for file in filelist])
+
+    for file in filelist:
+        z = accim_Main.accimobj_SingleZone_Ep91(file)
+
+        z.setComfFieldsPeople()
+        z.addOpTempTherm()
+        z.addBaseSchedules()
+        z.setAvailSchOn()
+
+        z.addForscriptSchSingleZone()
+
+        z.addEMSProgramsBase()
+        z.addEMSOutputVariableBase()
+
+        z.addGlobVarListSingleZone()
+        z.addEMSSensorsSingleZone()
+        z.addEMSActuatorsSingleZone()
+        z.addEMSProgramsSingleZone()
+        z.addEMSPCMBase()
+        z.addOutputVariablesSingleZone()
+
+        z.saveaccim()
+
+    z = accim_Main.accimobj()
+    z.inputdataSingleZone()
+    z.genIDFSingleZone()
+
+
+def addAccisSingleZoneEp94():
+    """Add SingleZone accis."""
+    import accim.sim.accim_Main as accim_Main
+
+    from os import listdir
+
+    filelist = ([file for file in listdir() if file.endswith('.idf')
+                 and '_pymod' not in file])
+
+    filelist = ([file.split('.idf')[0] for file in filelist])
+
+    for file in filelist:
+        z = accim_Main.accimobj_SingleZone_Ep94(file)
+
+        z.setComfFieldsPeople()
+        z.addOpTempTherm()
+        z.addBaseSchedules()
+        z.setAvailSchOn()
+
+        z.addForscriptSchSingleZone()
+
+        z.addEMSProgramsBase()
+        z.addEMSOutputVariableBase()
+
+        z.addGlobVarListSingleZone()
+        z.addEMSSensorsSingleZone()
+        z.addEMSActuatorsSingleZone()
+        z.addEMSProgramsSingleZone()
+        z.addEMSPCMBase()
+        z.addOutputVariablesSingleZone()
+
+        z.saveaccim()
+
+    z = accim_Main.accimobj()
+    z.inputdataSingleZone()
+    z.genIDFSingleZone()
+
+
+def addAccisSingleZoneTimestepEp91():
+    """Add SingleZone accis with timestep output data frecuency."""
+    import accim.sim.accim_Main as accim_Main
+
+    from os import listdir
+
+    filelist = ([file for file in listdir() if file.endswith('.idf')
+                 and '_pymod' not in file])
+
+    filelist = ([file.split('.idf')[0] for file in filelist])
+
+    for file in filelist:
+        z = accim_Main.accimobj_SingleZone_Ep91(file)
+
+        z.setComfFieldsPeople()
+        z.addOpTempTherm()
+        z.addBaseSchedules()
+        z.setAvailSchOn()
+
+        z.addForscriptSchSingleZone()
+
+        z.addEMSProgramsBase()
+        z.addEMSOutputVariableBase()
+
+        z.addGlobVarListSingleZone()
+        z.addEMSSensorsSingleZone()
+        z.addEMSActuatorsSingleZone()
+        z.addEMSProgramsSingleZone()
+        z.addEMSPCMBase()
+        z.addOutputVariablesSingleZone()
+        z.addOutputVariablesTimestep()
+
+        z.saveaccim()
+
+    z = accim_Main.accimobj()
+    z.inputdataSingleZone()
+    z.genIDFSingleZone()
+
+
+def addAccisSingleZoneTimestepEp94():
+    """Add SingleZone accis with timestep output data frecuency."""
+    import accim.sim.accim_Main as accim_Main
+
+    from os import listdir
+
+    filelist = ([file for file in listdir() if file.endswith('.idf')
+                 and '_pymod' not in file])
+
+    filelist = ([file.split('.idf')[0] for file in filelist])
+
+    for file in filelist:
+        z = accim_Main.accimobj_SingleZone_Ep94(file)
+
+        z.setComfFieldsPeople()
+        z.addOpTempTherm()
+        z.addBaseSchedules()
+        z.setAvailSchOn()
+
+        z.addForscriptSchSingleZone()
+
+        z.addEMSProgramsBase()
+        z.addEMSOutputVariableBase()
+
+        z.addGlobVarListSingleZone()
+        z.addEMSSensorsSingleZone()
+        z.addEMSActuatorsSingleZone()
+        z.addEMSProgramsSingleZone()
+        z.addEMSPCMBase()
+        z.addOutputVariablesSingleZone()
+        z.addOutputVariablesTimestep()
+
+        z.saveaccim()
+
+    z = accim_Main.accimobj()
+    z.inputdataSingleZone()
+    z.genIDFSingleZone()
+
+
+def addAccisSingleZoneSimplifiedEp91():
+    """Add SingleZone accis with simplified outputs."""
+    import accim.sim.accim_Main as accim_Main
+
+    from os import listdir
+
+    filelist = ([file for file in listdir() if file.endswith('.idf')
+                 and '_pymod' not in file])
+
+    filelist = ([file.split('.idf')[0] for file in filelist])
+
+    for file in filelist:
+        z = accim_Main.accimobj_SingleZone_Ep91(file)
+
+        z.setComfFieldsPeople()
+        z.addOpTempTherm()
+        z.addBaseSchedules()
+        z.setAvailSchOn()
+
+        z.addForscriptSchSingleZone()
+
+        z.addEMSProgramsBase()
+        z.addEMSOutputVariableBase()
+
+        z.addGlobVarListSingleZone()
+        z.addEMSSensorsSingleZone()
+        z.addEMSActuatorsSingleZone()
+        z.addEMSProgramsSingleZone()
+        z.addEMSPCMBase()
+        z.addSimplifiedOutputVariables()
+
+        z.saveaccim()
+
+    z = accim_Main.accimobj()
+    z.inputdataSingleZone()
+    z.genIDFSingleZone()
+
+
+def addAccisSingleZoneSimplifiedEp94():
+    """Add SingleZone accis with simplified outputs."""
+    import accim.sim.accim_Main as accim_Main
+
+    from os import listdir
+
+    filelist = ([file for file in listdir() if file.endswith('.idf')
+                 and '_pymod' not in file])
+
+    filelist = ([file.split('.idf')[0] for file in filelist])
+
+    for file in filelist:
+        z = accim_Main.accimobj_SingleZone_Ep94(file)
+
+        z.setComfFieldsPeople()
+        z.addOpTempTherm()
+        z.addBaseSchedules()
+        z.setAvailSchOn()
+
+        z.addForscriptSchSingleZone()
+
+        z.addEMSProgramsBase()
+        z.addEMSOutputVariableBase()
+
+        z.addGlobVarListSingleZone()
+        z.addEMSSensorsSingleZone()
+        z.addEMSActuatorsSingleZone()
+        z.addEMSProgramsSingleZone()
+        z.addEMSPCMBase()
+        z.addSimplifiedOutputVariables()
+
+        z.saveaccim()
+
+    z = accim_Main.accimobj()
+    z.inputdataSingleZone()
+    z.genIDFSingleZone()
+
+
+def addAccisMultipleZoneEp91():
+    """Add MultipleZone accis."""
+    import accim.sim.accim_Main as accim_Main
+
+    from os import listdir
+
+    filelist = ([file for file in listdir() if file.endswith('.idf')
+                 and '_pymod' not in file])
+
+    filelist = ([file.split('.idf')[0] for file in filelist])
+
+    for file in filelist:
+        z = accim_Main.accimobj_MultipleZone_Ep91(file)
+
+        z.setComfFieldsPeople()
+        z.addOpTempTherm()
+        z.addBaseSchedules()
+        z.setAvailSchOn()
+
+        z.addMultipleZoneSch()
+        z.addCurveObj()
+        z.addDetHVACobjEp91()
+        z.addForscriptSchMultipleZone()
+        z.checkVentIsOn()
+
+        z.addEMSProgramsBase()
+        z.addEMSOutputVariableBase()
+
+        z.addGlobVarListMultipleZone()
+        z.addEMSSensorsMultipleZone()
+        z.addEMSActuatorsMultipleZone()
+        z.addEMSProgramsMultipleZone()
+        z.addEMSPCMBase()
+        z.addEMSOutputVariableMultipleZone()
+        z.addOutputVariablesMultipleZone()
+
+        z.saveaccim()
+
+    z = accim_Main.accimobj()
+    z.inputdataMultipleZone()
+    z.genIDFMultipleZone()
+
+
+def addAccisMultipleZoneEp94():
+    """Add MultipleZone accis."""
+    import accim.sim.accim_Main as accim_Main
+
+    from os import listdir
+
+    filelist = ([file for file in listdir() if file.endswith('.idf')
+                 and '_pymod' not in file])
+
+    filelist = ([file.split('.idf')[0] for file in filelist])
+
+    for file in filelist:
+        z = accim_Main.accimobj_MultipleZone_Ep94(file)
+
+        z.setComfFieldsPeople()
+        z.addOpTempTherm()
+        z.addBaseSchedules()
+        z.setAvailSchOn()
+
+        z.addMultipleZoneSch()
+        z.addCurveObj()
+        z.addDetHVACobjEp94()
+        z.addForscriptSchMultipleZone()
+        z.checkVentIsOn()
+
+        z.addEMSProgramsBase()
+        z.addEMSOutputVariableBase()
+
+        z.addGlobVarListMultipleZone()
+        z.addEMSSensorsMultipleZone()
+        z.addEMSActuatorsMultipleZone()
+        z.addEMSProgramsMultipleZone()
+        z.addEMSPCMBase()
+        z.addEMSOutputVariableMultipleZone()
+        z.addOutputVariablesMultipleZone()
+
+        z.saveaccim()
+
+    z = accim_Main.accimobj()
+    z.inputdataMultipleZone()
+    z.genIDFMultipleZone()
+
+
+def addAccisMultipleZoneTimestepEp91():
+    """Add MultipleZone accis with timestep output data frecuency."""
+    import accim.sim.accim_Main as accim_Main
+
+    from os import listdir
+
+    filelist = ([file for file in listdir() if file.endswith('.idf')
+                 and '_pymod' not in file])
+
+    filelist = ([file.split('.idf')[0] for file in filelist])
+
+    for file in filelist:
+        z = accim_Main.accimobj_MultipleZone_Ep91(file)
+
+        z.setComfFieldsPeople()
+        z.addOpTempTherm()
+        z.addBaseSchedules()
+        z.setAvailSchOn()
+
+        z.addMultipleZoneSch()
+        z.addCurveObj()
+        z.addDetHVACobjEp91()
+        z.addForscriptSchMultipleZone()
+        z.checkVentIsOn()
+
+        z.addEMSProgramsBase()
+        z.addEMSOutputVariableBase()
+
+        z.addGlobVarListMultipleZone()
+        z.addEMSSensorsMultipleZone()
+        z.addEMSActuatorsMultipleZone()
+        z.addEMSProgramsMultipleZone()
+        z.addEMSPCMBase()
+        z.addEMSOutputVariableMultipleZone()
+        z.addOutputVariablesMultipleZone()
+        z.addOutputVariablesTimestep()
+
+        z.saveaccim()
+
+    z = accim_Main.accimobj()
+    z.inputdataMultipleZone()
+    z.genIDFMultipleZone()
+
+
+def addAccisMultipleZoneTimestepEp94():
+    """Add MultipleZone accis with timestep output data frecuency."""
+    import accim.sim.accim_Main as accim_Main
+
+    from os import listdir
+
+    filelist = ([file for file in listdir() if file.endswith('.idf')
+                 and '_pymod' not in file])
+
+    filelist = ([file.split('.idf')[0] for file in filelist])
+
+    for file in filelist:
+        z = accim_Main.accimobj_MultipleZone_Ep94(file)
+
+        z.setComfFieldsPeople()
+        z.addOpTempTherm()
+        z.addBaseSchedules()
+        z.setAvailSchOn()
+
+        z.addMultipleZoneSch()
+        z.addCurveObj()
+        z.addDetHVACobjEp94()
+        z.addForscriptSchMultipleZone()
+        z.checkVentIsOn()
+
+        z.addEMSProgramsBase()
+        z.addEMSOutputVariableBase()
+
+        z.addGlobVarListMultipleZone()
+        z.addEMSSensorsMultipleZone()
+        z.addEMSActuatorsMultipleZone()
+        z.addEMSProgramsMultipleZone()
+        z.addEMSPCMBase()
+        z.addEMSOutputVariableMultipleZone()
+        z.addOutputVariablesMultipleZone()
+        z.addOutputVariablesTimestep()
+
+        z.saveaccim()
+
+    z = accim_Main.accimobj()
+    z.inputdataMultipleZone()
+    z.genIDFMultipleZone()
+
+
+def addAccisMultipleZoneSimplifiedEp91():
+    """Add MultipleZone accis with simplified output."""
+    import accim.sim.accim_Main as accim_Main
+
+    from os import listdir
+
+    filelist = ([file for file in listdir() if file.endswith('.idf')
+                 and '_pymod' not in file])
+
+    filelist = ([file.split('.idf')[0] for file in filelist])
+
+    for file in filelist:
+        z = accim_Main.accimobj_MultipleZone_Ep91(file)
+
+        z.setComfFieldsPeople()
+        z.addOpTempTherm()
+        z.addBaseSchedules()
+        z.setAvailSchOn()
+
+        z.addMultipleZoneSch()
+        z.addCurveObj()
+        z.addDetHVACobjEp91()
+        z.addForscriptSchMultipleZone()
+        z.checkVentIsOn()
+
+        z.addEMSProgramsBase()
+        z.addEMSOutputVariableBase()
+
+        z.addGlobVarListMultipleZone()
+        z.addEMSSensorsMultipleZone()
+        z.addEMSActuatorsMultipleZone()
+        z.addEMSProgramsMultipleZone()
+        z.addEMSPCMBase()
+        z.addEMSOutputVariableMultipleZone()
+        z.addSimplifiedOutputVariables()
+
+        z.saveaccim()
+
+    z = accim_Main.accimobj()
+    z.inputdataMultipleZone()
+    z.genIDFMultipleZone()
+
+
+def addAccisMultipleZoneSimplifiedEp94():
+    """Add MultipleZone accis with simplified output."""
+    import accim.sim.accim_Main as accim_Main
+
+    from os import listdir
+    filelist = ([file for file in listdir() if file.endswith('.idf')
+                 and '_pymod' not in file])
+
+    filelist = ([file.split('.idf')[0] for file in filelist])
+
+    for file in filelist:
+        z = accim_Main.accimobj_MultipleZone_Ep94(file)
+
+        z.setComfFieldsPeople()
+        z.addOpTempTherm()
+        z.addBaseSchedules()
+        z.setAvailSchOn()
+
+        z.addMultipleZoneSch()
+        z.addCurveObj()
+        z.addDetHVACobjEp94()
+        z.addForscriptSchMultipleZone()
+        z.checkVentIsOn()
+
+        z.addEMSProgramsBase()
+        z.addEMSOutputVariableBase()
+
+        z.addGlobVarListMultipleZone()
+        z.addEMSSensorsMultipleZone()
+        z.addEMSActuatorsMultipleZone()
+        z.addEMSProgramsMultipleZone()
+        z.addEMSPCMBase()
+        z.addEMSOutputVariableMultipleZone()
+        z.addSimplifiedOutputVariables()
+
+        z.saveaccim()
+
+    z = accim_Main.accimobj()
+    z.inputdataMultipleZone()
+    z.genIDFMultipleZone()
