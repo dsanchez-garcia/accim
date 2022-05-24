01fd5ad6e48e24c046b2ca7019e74c0a7aabe86d
dsanchez-garcia <dsanchez7@us.es>
2022-05-24 18:57:01 +0200

    WIP

--- accim_japan_graphs_making graphs_v02.py
+++ accim_japan_graphs_making graphs_v02.py
@@ -31,10 +31,9 @@ df_backup = z.df.copy()
 
 
 ## Section 1
-#
 # df_fullAC = df_backup.copy()
 # # print(*df_fullAC, sep='\n')
-#
+# 
 # df_fullAC = df_fullAC[
 #     # (
 #     #     (df_fullAC['AdapStand'].isin(['AS_PMV']))
@@ -50,7 +49,7 @@ df_backup = z.df.copy()
 #             (df_fullAC['EPW_Scenario-Year'].isin(['Present']))
 #     )
 #     ]
-#
+# 
 # df_fullAC = df_fullAC[
 #     (
 #             (df_fullAC['AdapStand'].isin(['AS_ASHRAE55']))
@@ -74,9 +73,9 @@ df_backup = z.df.copy()
 #     #         (df_fullAC['AdapStand'].isin(['AS_PMV']))
 #     # )
 #     ]
-#
+# 
 # df_fullAC = df_fullAC.set_index([pd.RangeIndex(len(df_fullAC))])
-#
+# 
 # z.df = df_fullAC
 # z.wrangled_table(
 #     reshaping='unstack',
@@ -85,58 +84,118 @@ df_backup = z.df.copy()
 #     comparison_mode='baseline compared to others',
 #     comparison_cols=['relative', 'absolute']
 # )
-#
+# 
 # z.wrangled_df_unstacked.to_excel('section1_unstacked_test_9.xlsx')
 
 ## Section 2
+# df_MM = df_backup.copy()
+# # print(*df_MM, sep='\n')
+# 
+# df_MM = df_MM[
+#     (
+#         (df_MM['Category'].isin(['CA_80']))
+#         &
+#         (df_MM['EPW_Scenario-Year'].isin(['Present']))
+#     )
+#     &
+#     (
+#         (
+#                 (df_MM['AdapStand'].isin(['AS_ASHRAE55']))
+#                 &
+#                 (df_MM['ComfMod'].isin(['CM_3']))
+#                 &
+#                 (df_MM['HVACmode'].isin(['HM_0']))
+#         )
+#         |
+#         (
+#                 (df_MM['AdapStand'].isin(['AS_JPN']))
+#                 &
+#                 (df_MM['ComfMod'].isin(['CM_0']))
+#                 &
+#                 (df_MM['HVACmode'].isin(['HM_0']))
+#         )
+#         |
+#         (
+#                 (df_MM['AdapStand'].isin(['AS_JPN']))
+#                 &
+#                 (df_MM['ComfMod'].isin(['CM_3']))
+#                 &
+#                 (df_MM['HVACmode'].isin(['HM_2']))
+#         )
+# 
+#     )
+#     ]
+# 
+# df_MM = df_MM.set_index([pd.RangeIndex(len(df_MM))])
+# 
+# # dict_tree = {}
+# # for i in z.indexcols:
+# #     for j in range(len(df_MM)):
+# # df_MM = df_MM.set_index(z.indexcols)
+# # print(*df_MM.index, sep='\n')
+# 
+# z.df = df_MM
+# z.wrangled_table(
+#     reshaping='unstack',
+#     vars_to_gather=['AdapStand', 'ComfMod', 'HVACmode'],
+#     baseline='AS_JPN[CM_3[HM_2',
+#     comparison_mode='baseline compared to others',
+#     comparison_cols=['relative', 'absolute'],
+#     # check_index_and_cols=True,
+#     vars_to_keep=[
+#         'EPW_City_or_subcountry'
+#     ]
+# )
+# 
+# z.wrangled_df_unstacked.to_excel('section2_unstacked_test_00.xlsx')
 
-df_MM = df_backup.copy()
-# print(*df_MM, sep='\n')
+## Section 3
 
-df_MM = df_MM[
-    (
-        (df_MM['Category'].isin(['CA_80']))
-        &
-        (df_MM['EPW_Scenario-Year'].isin(['Present']))
-    )
+df_CC = df_backup.copy()
+# print(*df_CC, sep='\n')
+
+df_CC = df_CC[
+    (df_CC['Category'].isin(['CA_80']))
     &
     (
         (
-                (df_MM['AdapStand'].isin(['AS_ASHRAE55']))
+                (df_CC['AdapStand'].isin(['AS_ASHRAE55']))
                 &
-                (df_MM['ComfMod'].isin(['CM_3']))
+                (df_CC['ComfMod'].isin(['CM_3']))
                 &
-                (df_MM['HVACmode'].isin(['HM_0']))
+                (df_CC['HVACmode'].isin(['HM_0']))
         )
         |
         (
-                (df_MM['AdapStand'].isin(['AS_JPN']))
+                (df_CC['AdapStand'].isin(['AS_JPN']))
                 &
-                (df_MM['ComfMod'].isin(['CM_0']))
+                (df_CC['ComfMod'].isin(['CM_0']))
                 &
-                (df_MM['HVACmode'].isin(['HM_0']))
+                (df_CC['HVACmode'].isin(['HM_0']))
         )
         |
         (
-                (df_MM['AdapStand'].isin(['AS_JPN']))
+                (df_CC['AdapStand'].isin(['AS_JPN']))
                 &
-                (df_MM['ComfMod'].isin(['CM_3']))
+                (df_CC['ComfMod'].isin(['CM_3']))
                 &
-                (df_MM['HVACmode'].isin(['HM_2']))
+                (df_CC['HVACmode'].isin(['HM_2']))
         )
 
     )
     ]
 
-df_MM = df_MM.set_index([pd.RangeIndex(len(df_MM))])
+df_CC = df_CC.set_index([pd.RangeIndex(len(df_CC))])
 
 # dict_tree = {}
 # for i in z.indexcols:
-#     for j in range(len(df_MM)):
-# df_MM = df_MM.set_index(z.indexcols)
-# print(*df_MM.index, sep='\n')
+#     for j in range(len(df_CC)):
+# df_CC = df_CC.set_index(z.indexcols)
+# print(*df_CC.index, sep='\n')
 
-z.df = df_MM
+
+# V00
+z.df = df_CC
 z.wrangled_table(
     reshaping='unstack',
     vars_to_gather=['AdapStand', 'ComfMod', 'HVACmode'],
@@ -145,10 +204,33 @@ z.wrangled_table(
     comparison_cols=['relative', 'absolute'],
     # check_index_and_cols=True,
     vars_to_keep=[
-        'EPW_City_or_subcountry'
+        'EPW_City_or_subcountry',
+        'EPW_Scenario',
+        'EPW_Year'
     ]
 )
 
-z.wrangled_df_unstacked.to_excel('section2_unstacked_test_00.xlsx')
+z.wrangled_df_unstacked.to_excel('section3_unstacked_v00_01.xlsx')
 
 
+# V01
+z.df = df_CC
+z.wrangled_table(
+    reshaping='unstack',
+    vars_to_gather=[
+        'EPW_Scenario',
+        'EPW_Year'
+    ],
+    baseline='Present[Present',
+    comparison_mode='baseline compared to others',
+    comparison_cols=['relative', 'absolute'],
+    # check_index_and_cols=True,
+    vars_to_keep=[
+        'EPW_City_or_subcountry',
+        'AdapStand',
+        'ComfMod',
+        'HVACmode'
+    ]
+)
+
+z.wrangled_df_unstacked.to_excel('section3_unstacked_v01_01.xlsx')
