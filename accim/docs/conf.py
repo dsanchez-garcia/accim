47b62a70bd4b6a513d96b08e13842decf7745bf8
dsanchez-garcia <dsanchez7@us.es>
2023-06-21 11:00:37 +0200

    0.6.12 - docs and notebooks

--- accim/docs/conf.py
+++ accim/docs/conf.py
@@ -7,8 +7,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import accim
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
