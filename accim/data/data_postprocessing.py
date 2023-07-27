f0a32344e9500192fda3ac41f32573fe08f74b16
dsanchez-garcia <dsanchez7@us.es>
2023-07-27 17:28:51 +0200

    0.7.1 - ibpsa webinar

--- accim/data/data_postprocessing.py
+++ accim/data/data_postprocessing.py
@@ -1914,16 +1914,18 @@ class Table:
             try:
                 self.indexcols.remove('Source')
             except ValueError:
-                print('Since this is not the first time you run wrangled_table, '
-                      '"Source" is trying to be removed from indexcols, but has been previously removed.')
+                pass
+                # print('Since this is not the first time you run wrangled_table, '
+                #       '"Source" is trying to be removed from indexcols, but has been previously removed.')
             if self.split_epw_names:
                 wrangled_df_unstacked_or_stacked = wrangled_df_unstacked_or_stacked.drop(['EPW', 'EPW_Scenario-Year'], axis=1)
                 try:
                     self.indexcols.remove('EPW')
                     self.indexcols.remove('EPW_Scenario-Year')
                 except ValueError:
-                    print('Since this is not the first time you run wrangled_table, '
-                          '"EPW" and "EPW_Scenario-Year" are trying to be removed from indexcols, but has been previously removed.')
+                    pass
+                    # print('Since this is not the first time you run wrangled_table, '
+                    #       '"EPW" and "EPW_Scenario-Year" are trying to be removed from indexcols, but has been previously removed.')
 
             # removing variables where values are all the same
             cols_to_clean = []
