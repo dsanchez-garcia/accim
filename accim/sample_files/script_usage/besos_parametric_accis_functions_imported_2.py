9bdd0e62d5440a37616bf3b9df1d84d41095eda1
dsanchez-garcia <d.sanchez@zerocem.es>
2024-02-01 16:21:11 +0100

    0.7.3 - adding functions to operate with besos

--- accim/sample_files/script_usage/besos_parametric_accis_functions_imported_2.py
+++ accim/sample_files/script_usage/besos_parametric_accis_functions_imported_2.py
@@ -66,84 +66,9 @@ for i in comfstand_range:
 
             index_val = index_val + 1
 
-
-# samples = samples.dropna()
-#
-# valid_params = {
-#     0: [['n/a'], ['n/a']],
-#     1: [[1, 2, 3], [0, 1, 2, 3]],
-#     2: [[80, 90], [0, 1, 2, 3]],
-#     3: [[80, 90], [0, 1, 2, 3]],
-#     4: [[1, 2], [3]],
-#     5: [[1, 2], [3]],
-#     6: [[80, 90], [0, 1, 2, 3]],
-#     7: [[80, 85, 90], [0, 1, 2, 3]],
-#     8: [[80, 85, 90], [0, 1, 2, 3]],
-#     9: [[80, 90], [0, 1, 2, 3]],
-#     10: [[80, 90], [0, 1, 2, 3]],
-#     11: [[80, 90], [0, 1, 2, 3]],
-#     12: [[80, 90], [0, 1, 2, 3]],
-#     13: [[80, 90], [0.1, 0.2, 0.3, 0.4, 0.5, 1.1, 1.2, 1.3, 1.4, 1.5, 2, 3]],
-#     14: [[80, 90], [0.1, 0.2, 0.3, 0.4, 0.5, 1.1, 1.2, 1.3, 1.4, 1.5, 2, 3]],
-#     15: [[80, 90], [0, 1, 2, 3]],
-#     16: [[80, 90], [0, 1, 2, 3]],
-#     17: [[80, 90], [0, 1, 2, 3]],
-#     18: [[80, 90], [0, 1, 2, 3]],
-#     19: [[80, 90], [0, 1, 2, 3]],
-#     20: [[80, 90], [0, 1, 2, 3]],
-#     21: [[80, 90], [2, 3]],
-#     22: [[1, 2, 3], [0]],
-# }
-# samples['valid'] = 'temp'
-# for i in samples.index:
-#     if samples.loc[i, 'CAT'] not in valid_params[samples.loc[i, 'ComfStand']][0]:
-#         samples.loc[i, 'valid'] = False
-#     elif samples.loc[i, 'ComfMod'] not in valid_params[samples.loc[i, 'ComfStand']][1]:
-#         samples.loc[i, 'valid'] = False
-#     elif samples.loc[i, 'VentCtrl'] != 0 and samples.loc[i, 'HVACmode'] == 0:
-#         samples.loc[i, 'valid'] = False
-#     elif samples.loc[i, 'VSToffset'] != 0 and samples.loc[i, 'HVACmode'] == 0:
-#         samples.loc[i, 'valid'] = False
-#     elif samples.loc[i, 'MinOToffset'] != 0 and samples.loc[i, 'HVACmode'] == 0:
-#         samples.loc[i, 'valid'] = False
-#     elif samples.loc[i, 'MaxWindSpeed'] != 0 and samples.loc[i, 'HVACmode'] == 0:
-#         samples.loc[i, 'valid'] = False
-#     elif samples.loc[i, 'SetpointAcc'] < 0:
-#         samples.loc[i, 'valid'] = False
-#     elif samples.loc[i, 'MaxTempDiffVOF'] <= 0:
-#         samples.loc[i, 'valid'] = False
-#     elif samples.loc[i, 'MinTempDiffVOF'] <= 0:
-#         samples.loc[i, 'valid'] = False
-#     elif samples.loc[i, 'MinTempDiffVOF'] >= samples.loc[i, 'MaxTempDiffVOF'] <= 0:
-#         samples.loc[i, 'valid'] = False
-#     elif samples.loc[i, 'MultiplierVOF'] < 0 or samples.loc[i, 'MultiplierVOF'] > 1:
-#         samples.loc[i, 'valid'] = False
-#     else:
-#         samples.loc[i, 'valid'] = True
-#
-# samples_cleaned = samples[samples['valid'] == True]
-
 samples_filtered = bf.drop_invalid_param_combinations(samples)
 
 ##
-
-# adaptive_coeff_range = [round(i, 2) for i in np.arange(-1.0, 0, 0.1)]
-# adaptive_coeff_range.extend([round(i, 2) for i in np.arange(0, 1.1, 0.1)])
-# len(adaptive_coeff_range)
-#
-# pmv_range = [round(i, 2) for i in np.arange(0.3, 0.90, 0.05)]
-# len(pmv_range)
-# pmv_full = [[i]*len(adaptive_coeff_range) for i in pmv_range]
-# pmv_full = np.array(pmv_full).flatten().tolist()
-#
-# samples = pd.DataFrame(
-#     {
-#         "Adaptive coefficient": adaptive_coeff_range*len(pmv_range),
-#         "PMV": pmv_full,
-#     }
-# )
-#
-# samples_short = samples[:5]
 
 parameters = [
     Parameter(
@@ -201,22 +126,6 @@ problem = EPProblem(
     outputs=objectives
 )
 
-# inputs = sampling.dist_sampler(sampling.full_factorial, problem, num_samples=len(adaptive_coeff_range), level=len(pmv_range))
-# inputs
-
-# samples = pd.DataFrame(
-#     {
-#         "Thickness": [x / 10 for x in range(1, 10)] * 2,
-#         "Watts": [8, 10, 12] * 6,
-#         "wwr": [0.25, 0.5] * 9,
-#     }
-# )
-
-# bundle all of the different selectors into a single list of parameters
-
-# parameters = [
-#     Parameter(selector=x) for x in (insulation_idf, lights_selector, window_to_wall)
-# ]
 
 evaluator = EvaluatorEP(
     problem=problem,
