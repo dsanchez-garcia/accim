2eab3fa12d4d349392f214df52802523a6ae4148
dsanchez-garcia <dsanchez7@us.es>
2024-05-30 09:29:49 +0200

    0.7.5 - parametric simulation

--- accim/parametric/parametric_simulation_v01_accis_inside.py
+++ accim/parametric/parametric_simulation_v01_accis_inside.py
@@ -1,3 +1,6 @@
+import os
+import re
+
 import pandas as pd
 import warnings
 from besos import eppy_funcs as ef, sampling
@@ -100,7 +103,7 @@ class ParametricSimulation:
         )
 
         pass
-    def get_outputs_df_from_idf(self):
+    def get_output_var_df_from_idf(self):
         """
         Gets a pandas DataFrame which contains the Output:Variable objects from the idf.
         Therefore, it may contain wildcards such as '*', which means the variable is requested
@@ -119,13 +122,14 @@ class ParametricSimulation:
 
         return output_variable_df
 
-    def set_outputs_df_to_idf(self, outputs_df: pd.DataFrame = None):
+    def set_output_var_df_to_idf(self, outputs_df: pd.DataFrame = None):
         """
         Keeps the Output:Variable objects contained in the input pandas DataFrame and removes
         all others. This is important to save space if thousands of simulations with heavy outputs
         are run.
 
-        :param outputs_df:
+        :type outputs_df: pd.DataFrame
+        :param outputs_df: the DataFrame containing Output:Variable objects to be kept
         :return:
         """
         accis.addAccis(
@@ -143,6 +147,14 @@ class ParametricSimulation:
             # make_averages=True,
             # debugging=True
         )
+    def set_output_met_objects_to_idf(self, output_meters):
+        for meter in output_meters:
+            for freq in self.output_freqs:
+                building.newidfobject(
+                    key='OUTPUT:METER',
+                    Key_Name=meter,
+                    Reporting_Frequency=freq
+                )
 
     def get_outputs_df_from_testsim(self):
         """
@@ -158,28 +170,85 @@ class ParametricSimulation:
         )
         df_outputvariables = pd.DataFrame(
             available_outputs.variablereaderlist,
-            columns=['key_value', 'output:variable', 'frequency']
+            columns=['key_value', 'variable_name', 'frequency']
         )
 
         return df_outputmeters, df_outputvariables
 
-    def set_outputs_for_parametric_simulation(self, df_output_variable, df_output_meter):
-        pass
+    def get_rdd_file_as_df(self):
+        rdd_df = pd.read_csv(
+            filepath_or_buffer='available_outputs/eplusout.rdd',
+            sep=',|;',
+            skiprows=2,
+            names=['object', 'key_value', 'variable_name', 'frequency', 'units']
+        )
+        return rdd_df
 
+    def get_mdd_file_as_df(self):
+        mdd_df = pd.read_csv(
+            filepath_or_buffer='available_outputs/eplusout.mdd',
+            sep=',|;',
+            skiprows=2,
+            names=['object', 'meter_name', 'frequency', 'units']
+        )
+        return mdd_df
 
-##
+    def parse_mtd_file(self):
+        meter_list = []
+        with open('available_outputs/eplusout.mtd', 'r') as file:
+            lines = file.readlines()
+
+        meter_id, description = None, None
+        on_meters = []
+
+        for line in lines:
+            line = line.strip()
+            if line.startswith('Meters for'):
+                if meter_id is not None:
+                    meter_list.append({
+                        'meter_id': meter_id,
+                        'description': description,
+                        'on_meters': on_meters
+                    })
+                match = re.match(r'Meters for (\d+),(.+)', line)
+                if match:
+                    meter_id = match.group(1)
+                    description = match.group(2)
+                    on_meters = []
+            elif line.startswith('OnMeter'):
+                on_meters.append(line.split('=')[1].strip())
+
+        # Add the last meter
+        if meter_id is not None:
+            meter_list.append({
+                'meter_id': meter_id,
+                'description': description,
+                'on_meters': on_meters
+            })
+
+        return meter_list
+
+    def set_outputs_for_parametric_simulation(self, df_output_variable, output_meters):
+        objs_meters = [MeterReader(key_name=i, name=i) for i in output_meters]
+        objs_variables = []
+        for i in df_output_variable.index:
+            objs_variables.append(
+                    VariableReader(
+                        key_value=df_output_variable.loc[i, 'key_value'],
+                        variable_name=df_output_variable.loc[i, 'variable_name'],
+                        frequency=df_output_variable.loc[i, 'frequency'],
+                        name=df_output_variable.loc[i, 'variable_name'],
+                        # func=mean
+                    )
+                )
+        self.param_sim_outputs = objs_meters + objs_variables
+
 
 test_class_instance = ParametricSimulation(building=building)
 
-df_output_variables_idf = test_class_instance.get_outputs_df_from_idf()
-# output_variable_df = accis.gen_outputs_df(
-#     idf=building,
-#     ScriptType='vrf_mm',
-#     Output_keep_existing=False,
-#     Output_type='standard',
-#     Output_freqs=['hourly'],
-#     TempCtrl='temperature',
-# )
+# Setting the Output:Variable objects in the idf
+#todo do not print on screen the process of accis, only the first time
+df_output_variables_idf = test_class_instance.get_output_var_df_from_idf()
 
 df_output_variables_idf_mod = df_output_variables_idf.copy()
 df_output_variables_idf_mod = df_output_variables_idf_mod[
@@ -190,10 +259,41 @@ df_output_variables_idf_mod = df_output_variables_idf_mod[
     )
 ]
 
-test_class_instance.set_outputs_df_to_idf(outputs_df=df_output_variables_idf_mod)
+test_class_instance.set_output_var_df_to_idf(outputs_df=df_output_variables_idf_mod)
 
+output_meters = [
+    'HeatingCoils:EnergyTransfer',
+    'CoolingCoils:EnergyTransfer',
+    'Heating:Electricity',
+    'Cooling:Electricity',
+    'Electricity:HVAC',
+]
+test_class_instance.set_output_met_objects_to_idf(output_meters=output_meters)
 
+# Checking the Output:Meter and Output:Variable objects in the simulation
 df_outputmeters_2, df_outputvariables_2 = test_class_instance.get_outputs_df_from_testsim()
+
+#Other variables could be reported. These can be read in the rdd, mdd and mtd files
+df_rdd = test_class_instance.get_rdd_file_as_df()
+df_mdd = test_class_instance.get_mdd_file_as_df()
+meter_list = test_class_instance.parse_mtd_file()
+
+# To end with outputs, let's set the objective outputs (outputs for the Problem object), which are those displayed by BESOS in case of parametric analysis, or used in case of optimisation
+
+test_class_instance.set_outputs_for_parametric_simulation(
+    output_meters=output_meters,
+    df_output_variable=df_outputvariables_2
+)
+
+
+# At this point, the outputs of each energyplus simulation has been set. So, next step is setting parameters
+
+
+##
+
+
+
+
 
 ##
 # remove_accents_in_idf(idf_path=idf_path)
@@ -202,12 +302,6 @@ df_outputmeters_2, df_outputvariables_2 = test_class_instance.get_outputs_df_fro
 # [i.Variable_Name for i in building.idfobjects['output:variable'] if 'Occupied Discomfortable' in i.Variable_Name]
 
 
-for meter in output_meters:
-    building.newidfobject(
-        key='OUTPUT:METER',
-        Key_Name=meter,
-        Reporting_Frequency='hourly'
-    )
 
 [i.Variable_Name for i in building.idfobjects['output:variable']]
 [i for i in building.idfobjects['output:variable']]
@@ -232,7 +326,6 @@ bf.modify_MaxTempDiffVOF(building, 6)
 [i for i in building.idfobjects['EnergyManagementSystem:Program'] if i.Name.lower() == 'applycat']
 
 
-available_outputs = print_available_outputs_mod(building)
 
 parameters_list = [params.accis_parameter(k, v) for k, v in accis_parameters.items()]
 parameters_list.extend(other_parameters)
@@ -250,29 +343,10 @@ def mean(result):
 
 # Objectives
 
-objs_variables = []
-for i in range(len(available_outputs.variablereaderlist)):
-    if (
-            'Adaptive Cooling Setpoint Temperature_No Tolerance' == available_outputs.variablereaderlist[i][1] or
-            'Adaptive Heating Setpoint Temperature_No Tolerance' == available_outputs.variablereaderlist[i][1] or
-            'Zone Operative Temperature' == available_outputs.variablereaderlist[i][1] or
-            'AFN Zone Infiltration Air Change Rate' == available_outputs.variablereaderlist[i][1] or
-            'AFN Zone Ventilation Air Change Rate' == available_outputs.variablereaderlist[i][1]
-
-    ):
-        objs_variables.append(
-            VariableReader(
-                key_value=available_outputs.variablereaderlist[i][0],
-                variable_name=available_outputs.variablereaderlist[i][1],
-                frequency=available_outputs.variablereaderlist[i][2],
-                name=available_outputs.variablereaderlist[i][1],
-                func=mean
-            )
-        )
 
 
 
-objs_meters = [MeterReader(key_name=i, name=i) for i in output_meters]
+
 
 # obj_avg = [MeterReader(key_name='TOTAL OCCUPIED DISCOMFORTABLE HOURS', func=avg, name='AVERAGE OCCUPIED DISCOMFORTABLE HOURS')]
 
