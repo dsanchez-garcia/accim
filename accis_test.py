39fef31cc31581804ee6016fb8cc393d33e83f0f
Daniel Sánchez-García <daniel.sanchezgarcia@uca.es>
2026-04-13 14:52:12 +0200

    testing ex_mm

--- accis_test.py
+++ accis_test.py
@@ -1,16 +1,19 @@
 # from accim.utils import update_idf_version
-# update_idf_version(
-#     input_idf_path='OSM_SmallOffice_noHVAC_always-occ_V2320.idf',
-#     ep_version_target='25.1.0',
-#     output_idf_name ='OSM_SmallOffice_noHVAC_always-occ_V{version}.idf'
-# )
+# from os import listdir
+# idfs = [i for i in listdir() if i.endswith('.idf')]
+# for idf in idfs:
+#     update_idf_version(
+#         input_idf_path=idf,
+#         ep_version_target='25.1.0',
+#         # output_idf_name ='OSM_SmallOffice_noHVAC_always-occ_V{version}.idf'
+#     )
 
 ##
 
 from accim.sim import accis
 accis.addAccis(
-    idfs=['TestResidentialUnit_v01_onlygeometry_SchNatVent.idf'],
-    ScriptType='vrf_mm',
+    idfs=['TestModel_ExistingHVAC_PTAC.idf'],
+    ScriptType='ex_mm',
     SupplyAirTempInputMethod='temperature difference',
     Output_keep_existing=False,
     Output_type='standard',
@@ -36,7 +39,10 @@ accis.addAccis(
 from besos.eplus_funcs import run_building, run_energyplus
 
 run_energyplus(
-    building_path='TestResidentialUnit_v01_onlygeometry_SchNatVent[CS_AUS DeDear[CA_80[CM_3[HM_2[VC_0[VO_0.0[MT_50.0[MW_50.0[AT_0.1[NS_X.idf',
+    # building_path='TestResidentialUnit_v01_onlygeometry_SchNatVent[CS_AUS DeDear[CA_80[CM_3[HM_2[VC_0[VO_0.0[MT_50.0[MW_50.0[AT_0.1[NS_X.idf',
+    # building_path='OSM_SmallOffice_exHVAC_always-occ.idf',
+    # building_path='TestModel_ExistingHVAC_PTAC.idf',
+    building_path='TestModel_ExistingHVAC_PTAC[CS_AUS DeDear[CA_80[CM_3[HM_2[VC_0[VO_0.0[MT_50.0[MW_50.0[AT_0.1[NS_X.idf',
     epw='Sydney.epw',
-    out_dir='temp_output_dir_8'
+    out_dir='temp_output_dir_9'
 )
